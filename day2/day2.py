c67a1b572bb836649263ab47796b18e25567e721
Svante Pihl <hej@svantepihl.com>
2020-12-02 10:26:49 +0100

    day 2 finished

--- day2/day2.py
+++ day2/day2.py
@@ -22,17 +22,17 @@ How many passwords are valid according to their policies?
 '''
 
 # Counts the number of valid passwords
-def count_valid_psws(psw_list):
+def count_valid_psws(psw_list,validation_function):
     valid_psws = 0
     for input in psw_list:
         rule, psw = input.split(':')
         rule, psw = rule.strip(),psw.strip()
-        if validate_psw(rule,psw):
+        if validation_function(rule,psw):
             valid_psws = valid_psws + 1
     return valid_psws
 
 # Checks if password adhers to rule, returns true if valid and false if not.
-def validate_psw(rule,psw):
+def validate_psw_p1(rule,psw):
     result = False
     key_char_count = 0
     count, key_char = rule.split()
@@ -48,7 +48,7 @@ def validate_psw(rule,psw):
 file = open('day2/input.txt','r')
 input_list = file.read().split('\n')
 
-print("Number of valid passwords:" + str(count_valid_psws(input_list)))
+print("Number of valid passwords:" + str(count_valid_psws(input_list,validate_psw_p1)))
 
 
 '''
@@ -58,7 +58,8 @@ While it appears you validated the passwords correctly, they don't seem to be wh
 
 The shopkeeper suddenly realizes that he just accidentally explained the password policy rules from his old job at the sled rental place down the street! The Official Toboggan Corporate Policy actually works a little differently.
 
-Each policy actually describes two positions in the password, where 1 means the first character, 2 means the second character, and so on. (Be careful; Toboggan Corporate Policies have no concept of "index zero"!) Exactly one of these positions must contain the given letter. Other occurrences of the letter are irrelevant for the purposes of policy enforcement.
+Each policy actually describes two positions in the password, where 1 means the first character, 2 means the second character, and so on. 
+(Be careful; Toboggan Corporate Policies have no concept of "index zero"!) Exactly one of these positions must contain the given letter. Other occurrences of the letter are irrelevant for the purposes of policy enforcement.
 
 Given the same example list from above:
 
@@ -67,3 +68,28 @@ Given the same example list from above:
 2-9 c: ccccccccc is invalid: both position 2 and position 9 contain c.
 How many passwords are valid according to the new interpretation of the policies?
 '''
+# Checks if password adhers to rule, returns true if valid and false if not.
+def validate_psw_p2(rule,psw):
+    result = False
+    psw_len = len(psw)
+    index, key_char = rule.split()
+    index1,index2 = index.split('-')
+    index1,index2 = int(index1), int(index2)
+    index1_valid = checkIndex(index1,key_char,psw)
+    index2_valid = checkIndex(index2,key_char,psw)
+    
+    if (index1_valid and not index2_valid) or (not index1_valid and index2_valid):
+        result = True
+    return result
+
+# Checks if index is not out-of-bounds and if index position in string is equal to char, if so returns true otherwise returns false. 
+def checkIndex(index,char,string):
+    result = False
+    if index > len(string):
+        return result
+    elif string[index-1] == char:
+        result = True
+    return result
+
+
+print("Number of valid passwords:" + str(count_valid_psws(input_list,validate_psw_p2)))
